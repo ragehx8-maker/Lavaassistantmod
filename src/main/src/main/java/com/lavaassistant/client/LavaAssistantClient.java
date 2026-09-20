367192680ff35a5afd65a1e228f4dde5e2041f70
ragehx8-maker <ragehx8@gmail.com>
2026-09-20 10:45:03 +0530

    Create LavaAssistantClient.java

---
+++ src/main/src/main/java/com/lavaassistant/client/LavaAssistantClient.java
@@ -0,0 +1,38 @@
+package com.lavaassistant.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class LavaAssistantClient implements ClientModInitializer {
+    public static boolean xrayEnabled = false;
+    private static KeyBinding xrayKeyBind;
+
+    @Override
+    public void onInitializeClient() {
+        xrayKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.lavaassistant.xray",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_X,
+                "category.lavaassistant.client"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (xrayKeyBind.wasPressed()) {
+                xrayEnabled = !xrayEnabled;
+                if (client.player != null) {
+                    client.player.sendMessage(
+                        net.minecraft.text.Text.literal("§6[LavaAssistant] X-Ray: " + (xrayEnabled ? "§aON" : "§cOFF")), 
+                        true
+                    );
+                }
+                if (client.worldRenderer != null) {
+                    client.worldRenderer.reload();
+                }
+            }
+        });
+    }
+}
